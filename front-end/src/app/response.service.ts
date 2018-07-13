a7563ca65b91ba9d26c21efd59fe4dae232390e5
jacobmeyerson <jacobmeyerson@college.harvard.edu>
2018-07-13 14:42:24 +0300

    front end gets survey from backend

--- front-end/src/app/response.service.ts
+++ front-end/src/app/response.service.ts
@@ -4,4 +4,8 @@ import { Http } from '@angular/http';
 @Injectable()
 export class ResponseService {
     constructor(private http: Http) {}
+
+    getSurveys() {
+        return this.http.get('http://localhost:3000/getSurveys');
+    }
 }
