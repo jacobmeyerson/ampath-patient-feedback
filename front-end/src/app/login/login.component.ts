9f76a3c3293382a9530bb475426912a5210468c9
jacobmeyerson <jacobmeyerson@college.harvard.edu>
2018-07-13 13:57:50 +0300

    commentig out login

--- front-end/src/app/login/login.component.ts
+++ front-end/src/app/login/login.component.ts
@@ -1,53 +1,54 @@
 import { Component, Output, EventEmitter, OnInit } from '@angular/core';
-import { Headers, Http, Response } from '@angular/http';
+// import { Headers, Http, Response } from '@angular/http';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
   @Output() loggedInComplete = new EventEmitter<boolean>();
 
-  constructor(private http: Http) { }
+  // constructor(private http: Http) { }
 
   onLogin(username, password) {
-    const headers = new Headers();
+    this.loggedInComplete.emit(true);
+    // const headers = new Headers();
 
-    const credentials = btoa(username.value + ':' + password.value);
+    // const credentials = btoa(username.value + ':' + password.value);
 
-    headers.append('Authorization', 'Basic ' + credentials);
+    // headers.append('Authorization', 'Basic ' + credentials);
 
-    const url = 'https://ngx.ampath.or.ke/test-amrs/ws/rest/v1/session/';
+    // const url = 'https://ngx.ampath.or.ke/test-amrs/ws/rest/v1/session/';
 
-    const request = this.http.get(url, {
-      headers: headers
-    });
+    // const request = this.http.get(url, {
+    //   headers: headers
+    // });
 
-    request
-    .subscribe(
-      (response: Response) => {
-        const data = response.json();
-        if (data.authenticated) {
-          this.loggedInComplete.emit(true);
-          this.http.delete(url, {
-            headers: headers}); // logs out - according to https://wiki.openmrs.org/display/docs/REST+Web+Services+API+For+Clients
-      }
-    });
+    // request
+    // .subscribe(
+    //   (response: Response) => {
+    //     const data = response.json();
+    //     if (data.authenticated) {
+    //       this.loggedInComplete.emit(true);
+    //       this.http.delete(url, {
+    //         headers: headers}); // logs out - according to https://wiki.openmrs.org/display/docs/REST+Web+Services+API+For+Clients
+    //   }
+    // });
   }
 
-  ngOnInit () {
-    const headers = new Headers();
+  // ngOnInit () {
+  //   const headers = new Headers();
 
-    const credentials = btoa('john:studio');
+  //   const credentials = btoa('john:studio');
 
-    headers.append('Authorization', 'Basic ' + credentials);
+  //   headers.append('Authorization', 'Basic ' + credentials);
 
-    const request = this.http.get('http://localhost:8000/reset', {
-      headers: headers
-    });
-    request.subscribe(
-      (response: Response) => console.log('Aunthentication is successful')
-    );
-  }
+  //   const request = this.http.get('http://localhost:8000/reset', {
+  //     headers: headers
+  //   });
+  //   request.subscribe(
+  //     (response: Response) => console.log('Aunthentication is successful')
+  //   );
+  // }
 }
