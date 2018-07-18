f233d9fbd4100bf0a0d0c1a229708add1e7628ae
jacobmeyerson <jacobmeyerson@college.harvard.edu>
2018-07-18 12:23:22 +0300

    got last insert id

--- back-end/index.js
+++ back-end/index.js
@@ -9,7 +9,7 @@ const connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
   password : 'jacob922',
-  database : 'patient_feedback'
+  database : 'patient_feedback2'
 });
 
 const server = Hapi.Server({
@@ -31,25 +31,64 @@ server.route({
 server.route({
   method: 'POST',
   path: '/storeSurveys',
-  handler: function(request, _h) {  
-    console.log(request.payload);
-    const q1 = request.payload.question1;
+  handler: function(request, h) {
+    const payload = request.payload;
 
+    const surveyId = payload.surveyId;
+    const location = payload.location;
+    const date = payload.date;
+    const department = payload.department;
+    const clinicalProgramId = payload.clinicalProgramId;
+    const surveyEncounter_query = `INSERT INTO surveyEncounter
+                                  (surveyId, location, date, department, clinicalProgramId) VALUES
+                                  ('${surveyId}','${location}','${date}','${department}','${clinicalProgramId}');`;
+    // console.log(surveyEncounter_query);
+    // const q1 = request.payload.question1;
     // TODO: check if whether it is question2a or question2b
-    const q2 = request.payload.question2.toString();
-
+    // const q2 = request.payload.question2.toString();
+    const surveyResponse_query = `INSERT INTO surveyResponse 
+                                  (surveyEncounter_surveyEncounterId, question, answer) VALUES 
+                                  ('25', 'TEST BOB', 'TEST BOB ANSWER');`;
+    const query = surveyEncounter_query + surveyResponse_query;
+    // `INSERT INTO responses (rating, reasons) VALUES (${q1},'${q2}');`;
     return new Promise(
       (resolve, reject) => {
+        // console.log(query);
         connection.query(
-          `INSERT INTO responses (rating, reasons) VALUES (${q1},'${q2}');`,
+          surveyEncounter_query,
           (error, _rows, _fields) => {
-            if (error) {console.log('ERROR'); reject(error)}
-          });
+            if (error) {console.log(error); reject(error)}
+          }
+        );
+        connection.query(
+          'SELECT LAST_INSERT_ID();',
+          (error, rows, _fields) => {
+            console.log(rows);
+          }
+        );
       }
     );
 
   }
 });
+server.route({
+  method: 'GET',
+  path: '/test',
+  handler: function(request, h) { 
+    const query1 = 'SELECT * FROM surveyResponse';
+    return new Promise(
+      (resolve, reject) => {
+        connection.query(
+          'SELECT * FROM surveyEncounter; SELECT * FROM surveyResponse',
+          (error, rows, _fields) => {
+            resolve(rows);
+          });
+      }
+    );
+  }
+});
+
+
 
 const init = async () => {
   connection.connect();
