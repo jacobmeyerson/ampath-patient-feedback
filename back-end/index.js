fe7617512727336c0f76b499e7997de0c7cdb1aa
jacobmeyerson <jacobmeyerson@college.harvard.edu>
2018-07-20 15:52:32 +0300

    factored out routes

--- back-end/index.js
+++ back-end/index.js
@@ -2,8 +2,6 @@
 
 'use strict';
 
-const SURVEYFILE = './surveys.json';
-
 const Hapi = require('hapi');
 const mysql = require('mysql');
 const request = require('request');
@@ -23,42 +21,6 @@ const server = Hapi.Server({
     cors: true
   }
 });
-
-
-// builds a query to insert appropriate number of rows into surveyResponse table
-const surveyResponse_query_constructor = (surveyResponse, surveyEncounterId) => {
-  const responseValue_constructor = (surveyEncounterId) => (question) => (answer) =>
-    "('"+surveyEncounterId+"','"+question+"','"+answer+"')";
-  const responseValue_surveySpecific = responseValue_constructor(surveyEncounterId);
-  const queryInit = (responseValues) => 
-                      'INSERT INTO surveyResponse (surveyEncounter_surveyEncounterId, question, answer) VALUES ' + 
-                      responseValues + ';';
-  
-  var responseValueArray = [];
-  for (var question in surveyResponse) {
-    if (!Array.isArray(surveyResponse[question])) {
-      responseValueArray.push(responseValue_surveySpecific(question)(surveyResponse[question]));
-    } else {
-      const responseValue_questionSpecific = responseValue_surveySpecific(question);
-      for (var answer of surveyResponse[question]) {
-        responseValueArray.push(responseValue_questionSpecific(answer));
-      }
-    }
-  }
-
-  return queryInit(responseValueArray.join())
-};
-
-const surveyEncounter_query_constructor = (surveyEncounterInfo) => {
-  const surveyEncounter_query = `INSERT INTO surveyEncounter
-                                (surveyId, location, date, department, clinicalProgramId) VALUES
-                                ('${surveyEncounterInfo.surveyId}',
-                                 '${surveyEncounterInfo.location}',
-                                 '${surveyEncounterInfo.date}',
-                                 '${surveyEncounterInfo.department}',
-                                 '${surveyEncounterInfo.clinicalProgramId}');`;
-  return surveyEncounter_query;
-};
 
 const validate = async (_request, username, password) => {
   var authBuffer = new Buffer(username + ":" + password).toString("base64");
@@ -84,47 +46,6 @@ const validate = async (_request, username, password) => {
     // });
   };
 
-server.route({
-  method: 'GET',
-  path: '/getSurveys',
-  handler: function(request, h) { 
-    return h.file(SURVEYFILE);
-  }
-});
-
-server.route({
-  method: 'POST',
-  path: '/storeSurveys',
-  handler: function(request, h) {
-    return new Promise(
-      (resolve, reject) => {
-        connection.query(
-          surveyEncounter_query_constructor(request.payload.encounterInfo),
-          (error, _rows, _fields) => {
-            if (error) {reject(error)}
-          }
-        );
-        connection.query(
-          'SELECT LAST_INSERT_ID();',
-          (error, rows, _fields) => {
-            if (error) {reject(error)}
-            const surveyEncounterId = rows[0]["LAST_INSERT_ID()"];
-            const surveyResponse_query = surveyResponse_query_constructor(request.payload.responseInfo, surveyEncounterId);
-            
-            connection.query(
-              surveyResponse_query,
-              (error, rows, _fields) => {
-                if (error) {reject(error)}
-              }
-            );
-          }
-        );
-      }
-    );
-  }
-});
-
-
 const init = async () => {
   connection.connect();
   
@@ -140,7 +61,7 @@ const init = async () => {
   }
 
   // causes all routes to require authentication
-  server.auth.default('simple');
+  // server.auth.default('simple');
 
   await server.start();
   console.log('Server is running');
